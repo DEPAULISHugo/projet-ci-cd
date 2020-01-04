11cf10e3f9925c525660f54ba5d1e48050881953
DEPAULISHugo <hugo.depaulis@gmail.com>
2020-01-04 12:40:30 +0100

    Ajout de la classe Products

--- src/main/java/eu/ensg/project/web/controller/ProductController.java
+++ src/main/java/eu/ensg/project/web/controller/ProductController.java
@@ -6,16 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import eu.ensg.project.model.Product;
+
 @RestController
 public class ProductController {
-	@RequestMapping(value="/Produits", method=RequestMethod.GET)
-    public String listeProduits() {
-        return "Un exemple de produit";
-    }
 	
-	@GetMapping(value = "/Produits/{id}")
-	public String afficherUnProduit(@PathVariable int id) {
-		return "Vous avez demandé un produit avec l'id  " + id;
-	}
+    // Récupérer la liste des produits
+    @RequestMapping(value="/Produits", method=RequestMethod.GET)
+    public String listeProduits() {
+        return"Un exemple de produit";
+    }
+
+    // Récupérer un produit par son Id
+    @GetMapping(value="/Produits/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+        Product product=new Product(id, new String("Aspirateur"), 100 );
+        return product;
+    }
 
 }
